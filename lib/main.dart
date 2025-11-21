a3cf8f54ca207f85f8a5626e126521b7ddce60d1
palmsbrito <paloma.brito020@gmail.com>
2025-11-21 17:46:47 -0300

    feat: criação do banco de dados no supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa o Supabase
+  await Supabase.initialize(
+    url: 'https://apsacslumdfaacjxyarz.supabase.co', // coloque o URL DO SEU PROJETO
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwc2Fjc2x1bWRmYWFjanh5YXJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3NTIzOTksImV4cCI6MjA3OTMyODM5OX0.D9F5DJ8RYToXH9Eb3m3ATjWEd4W3WW3CBCa34RKXi2Y',          // coloque sua ANON KEY
+  );
+
   runApp(const MainApp());
 }
 
